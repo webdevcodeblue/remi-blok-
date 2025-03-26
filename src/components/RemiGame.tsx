151309e653c3e8370532c9e4949c58483aba0102
Remi Player <remi.player@example.com>
2025-03-26 10:05:03 +0100

    commit-4.1

--- src/components/RemiGame.tsx
+++ src/components/RemiGame.tsx
@@ -1097,7 +1097,7 @@ const RemiGame: React.FC = () => {
                 onClick={() => {
                   if (
                     window.confirm(
-                      'Jeste li sigurni da želite završiti trenutnu igru? Podaci će biti sačuvani.'
+                      'Jeste li sigurni da želite završiti trenutnu igru? Podaci neće biti sačuvani.'
                     )
                   ) {
                     setGameStarted(false);
